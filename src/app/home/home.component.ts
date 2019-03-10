ce9eb7a9430721273be951590122b0796193c03b
Thai Bui <tbui64@gmail.com>
2019-03-09 22:12:56 -0500

    bind function to an event listener click events in home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,34 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  firstClick(){
+    alert("Click")
+  }
+
 }
+
+//Different types of click events.
+
+// (focus)="myMethod()"
+// (blur)="myMethod()" 
+// (submit)="myMethod()"  
+// (scroll)="myMethod()"
+
+// (cut)="myMethod()"
+// (copy)="myMethod()"
+// (paste)="myMethod()"
+
+// (keydown)="myMethod()"
+// (keypress)="myMethod()"
+// (keyup)="myMethod()"
+
+// (mouseenter)="myMethod()"
+// (mousedown)="myMethod()"
+// (mouseup)="myMethod()"
+
+// (click)="myMethod()"
+// (dblclick)="myMethod()"
+
+// (drag)="myMethod()"
+// (dragover)="myMethod()"
+// (drop)="myMethod()"
